116f16bec5491e6106e95fa8104c0cddd03194ea
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-09-25 01:08:05 +0900

    dom comment

--- public/content_script.js
+++ public/content_script.js
@@ -1,8 +1,11 @@
 /* global chrome */
 
 chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
+  const test = document.getElementById("wO nr l1");
+  console.log("test", test);
   const app = document.createElement("div");
   app.id = "my-extension-root";
   app.textContent = response;
-  document.body.appendChild(app);
+  test.insertBefore(app);
+  // document.body.appendChild(app);
 });
