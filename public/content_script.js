36450c2a35953bfb332100df6280857dcb725dbf
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-09-27 15:12:18 +0900

    mail context add

--- public/content_script.js
+++ public/content_script.js
@@ -1,13 +1,13 @@
 /* global chrome */
 
 chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
-  let test = document.getElementsByTagName("h1")[0];
+  let test = document.getElementById(":qi");
   console.log("test", test);
-  console.log(test.textContent);
-  test.textContent = response;
-  const app = document.createElement("div");
-  app.id = "my-extension-root";
-  app.textContent = response;
-  document.body.appendChild(app);
+  test.innerHTML = "test";
+  // test.textContent = response;
+  // const app = document.createElement("div");
+  // app.id = "my-extension-root";
+  // app.textContent = response;
+  // document.body.appendChild(app);
   // document.body.appendChild(app);
 });
