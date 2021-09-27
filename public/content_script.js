44ce5b2f0573244627a33bfa263d16d184f90981
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-09-27 15:40:50 +0900

    mail address

--- public/content_script.js
+++ public/content_script.js
@@ -1,9 +1,15 @@
 /* global chrome */
 
 chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
-  let test = document.getElementById(":qi");
-  console.log("test", test);
-  test.innerText = response;
+  // アドレスを変更する方法
+  let test = document.getElementById(":pi");
+  test.children[0].children[1].innerText = response;
+
+  // メール本文を変更する方法
+  // let test = document.getElementById(":qi");
+  // console.log("test", test);
+  // test.innerText = response;
+
   // test.textContent = response;
   // const app = document.createElement("div");
   // app.id = "my-extension-root";
