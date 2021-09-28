4c2f765632f63e6ad6fd78f55e2dd524595a4b3b
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-09-28 15:22:44 +0900

    finish

--- public/content_script.js
+++ public/content_script.js
@@ -1,67 +1,17 @@
 /* global chrome */
 
 chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
-  // アドレスを変更する方法
-  // 挿入設定
-  let test = document.getElementById(":pi").children[0];
-  console.log(test);
-  var reference = test.children[0];
-
-  // 挿入データ
-  var newEle = document.createElement("div");
-  var spanEle = document.createElement("span");
-  var newEle2 = document.createElement("div");
-  var newEle3 = document.createElement("div");
-  var inputEle = document.createElement("input");
-
-  // クラス追加
-  newEle.classList.add("vR");
-  spanEle.classList.add("vN");
-  spanEle.classList.add("bfK");
-  spanEle.classList.add("a3q");
-  spanEle.setAttribute("email", "kazumasa.matsumoto62@gmail.com");
-  spanEle.setAttribute("data-hovercard-id", "kazumasa.matsumoto62@gmail.com");
-  spanEle.setAttribute("data-hovercard-owner-id", "114");
-  spanEle.setAttribute("email", "kazumasa.matsumoto62@gmail.com");
-
-  // spanの中
-  newEle2.setAttribute("translate", "no");
-  newEle2.classList.add("vT");
-  newEle2.innerText = "kazumasa.matsumoto62@gmail.com";
-  spanEle.appendChild(newEle2);
-
-  // spanの中その２
-  newEle3.classList.add("vM");
-  spanEle.appendChild(newEle3);
-
-  newEle.appendChild(spanEle);
-
-  // inputElementの追加
-  inputEle.setAttribute("name", "to");
-  inputEle.setAttribute("type", "hidden");
-  inputEle.setAttribute("value", "kazumasa.matsumoto62@gmail.com");
-  newEle.appendChild(inputEle);
-
-  // 挿入します
-  test.insertBefore(newEle, reference.nextSibling);
-
-  // test.children[0].children[1].innerHtml = "<div>sample</div>";
-  // メールアドレスの宛先の加工innerHTML
-  // textareaの上に
-  // メール送信ができるか。
-  // responseをオブジェクトにして
-  // それぞれのキーで取得する
-  // URLをコピーするタイミングでメールに転記
-
-  // メール本文を変更する方法
-  // let test = document.getElementById(":qi");
-  // console.log("test", test);
-  // test.innerText = response.url;
-
-  // test.textContent = response;
-  // const app = document.createElement("div");
-  // app.id = "my-extension-root";
-  // app.textContent = response;
-  // document.body.appendChild(app);
-  // document.body.appendChild(app);
+  let textareaAddress = document.getElementById(":pv");
+  textareaAddress.innerText = response.email;
+  let textareaContent = document.getElementById(":qi");
+  textareaContent.innerText = response.url;
 });
+
+// アドレスを変更する方法
+// 挿入設定
+// メールアドレスの宛先の加工innerHTML
+// textareaの上に
+// メール送信ができるか。
+// responseをオブジェクトにして
+// それぞれのキーで取得する
+// URLをコピーするタイミングでメールに転記
--- src/App.js
+++ src/App.js
@@ -3,31 +3,55 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState("");
   const [email, setEmail] = useState("");
+  const [urlLink, setUrlLink] = useState("");
+  const sample = { email: users, url: urlLink };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(email);
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, email);
+      chrome.tabs.sendMessage(tabs[0].id, sample);
     });
+  };
+
+  const addUsers = (address) => {
+    if (users === "") {
+      setUsers(address);
+    } else {
+      setUsers(users + ", " + address);
+    }
+    setEmail("");
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input type="submit" value="Submit" />
-          </label>
-        </form>
+        <label>
+          Email:
+          <input
+            type="text"
+            name="name"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={() => addUsers(email)}>addUser</button>
+        </label>
+        <br />
+        <br />
+        <label>
+          URL:
+          <input
+            type="text"
+            name="name"
+            value={urlLink}
+            onChange={(e) => setUrlLink(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <button onClick={(e) => handleSubmit(e)}>send</button>
+        <p>{users.length}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
